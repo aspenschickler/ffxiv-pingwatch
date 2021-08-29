561dd890f40fbf9bd50b63cbf74a3c79a110bfd2
zachschickler <zachschickler@knights.ucf.edu>
2021-08-29 00:22:01 -0400

    data center selection

--- PingWatch/PingWatch/MainPage.xaml.cs
+++ PingWatch/PingWatch/MainPage.xaml.cs
@@ -20,11 +20,25 @@ namespace PingWatch
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+
     public sealed partial class MainPage : Page
     {
+        private String[] data_center_ip_arr;
+        private TextBox tb;
+        private ComboBox cb;
+
         public MainPage()
         {
+            data_center_ip_arr = new string[8] { "204.2.229.10", "124.150.157.158", "124.150.157.157", "124.150.157.156", "204.2.229.9", "195.82.50.10", "195.82.50.9", "204.2.229.11" }; ;
+
             this.InitializeComponent();
+            tb = this.FindName("textBox") as TextBox;
+            cb = this.FindName("comboBox") as ComboBox;
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            tb.Text = data_center_ip_arr[cb.SelectedIndex];
         }
     }
 }
